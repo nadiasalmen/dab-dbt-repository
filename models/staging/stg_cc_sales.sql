cb3e9b823489adf523422465d6d939c33c278756
Nadia Salmen <nadiasalmen@gmail.com>
2023-09-09 12:49:25 +0000

    Creating a partitioned table

--- models/staging/stg_cc_sales.sql
+++ models/staging/stg_cc_sales.sql
@@ -1,3 +1,12 @@
+{{ config(
+    materialized='table',
+    partition_by={
+      "field": "date_date",
+      "data_type": "date",
+      "granularity": "day"
+    }
+)}}
+
 SELECT
   date_date
   ### Key ###  
